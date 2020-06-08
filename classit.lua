bf81dc8d99f39741dc225f58416b0a880c2ddb85
grynmoor <65985359+grynmoor@users.noreply.github.com>
2020-06-08 05:43:09 -0500

    Update classit.lua

--- classit.lua
+++ classit.lua
@@ -1,3 +1,29 @@
+-- Base methods that all classes will have
+-- :init(...) is used for constructing classes, being called immediately after the initialization of a new object
+-- All :init(...) methods in inheriting classes must start with class.super.init(self, ...) for things to work as intended!
+local function init(self, ...) return end
+-- Type-check method for classes
+local function is(self, tbl0)
+	local tbl1 = self.class
+	while tbl1 do
+		if tbl0 == tbl1 then return true end
+		tbl1 = tbl1.super
+	end
+	return false
+end	
+-- Minimal support for mixins
+local function mixin(self, ...)
+    for _, item in pairs({...}) do
+        for i, v in pairs(item) do
+            if self[i] == nil and type(v) == "function" then
+                self[i] = v
+            end
+        end
+    end
+end	
+
+-- Used to create new classes
+-- Is returned by module
 local function classit(superClass)
 	local class, objectMt = {}, {}
 	local classMt = {
@@ -28,28 +54,9 @@ local function classit(superClass)
 	else
 		-- If not inheriting, warn user of possible improper argument (if valid) and implement base methods
 		if superClass ~= nil then print(("superclass not set due to improper argument\n%s"):format(debug.traceback())) end
-		-- Called immediately after initialization of a new object
-		-- All :init(...) functions in inheriting classes must start with class.super.init(self, ...) for things to work as intended!
-		function class:init(...) return end
-		-- Type-check method for classes
-		function class:is(tbl0)
-			local tbl1 = self.class
-			while tbl1 do
-				if tbl0 == tbl1 then return true end
-				tbl1 = tbl1.super
-			end
-			return false
-		end	
-		-- Minimal support for mixins
-		function class:mixin(...)
-		    for _, item in pairs({...}) do
-		        for i, v in pairs(item) do
-		            if self[i] == nil and type(v) == "function" then
-		                self[i] = v
-		            end
-		        end
-		    end
-		end
+		class.init = init 
+		class.is = is 
+		class.mixin = mixin
 	end
 	-- Final touches to new class, add self-reference and object metatable, set '__index' for objectMt, and lastly set 'classMt' as a metatable for 'class'
 	class.class = class
@@ -57,4 +64,5 @@ local function classit(superClass)
 	objectMt.__index = class
 	return setmetatable(class, classMt)
 end
+
 return classit
