e3d57e1c1b5655a04e7d6876b1dd1076449ced61
grynmoor <65985359+grynmoor@users.noreply.github.com>
2025-03-08 14:58:41 -0600

    Update object.lua

--- object.lua
+++ object.lua
@@ -1,24 +1,24 @@
 
 local metamethods = { --- Lookup table used for filtering metamethods
-	__index = true;
-	__newindex = true;
-	__call = true;
-	__concat = true;
-	__unm = true;
-	__add = true;
-	__sub = true;
-	__mul = true;
-	__div = true;
-	__mod = true;
-	__pow = true;
-	__tostring = true;
-	__metatable = true;
-	__eq = true;
-	__lt = true;
-	__le = true;
-	__mode = true;
-	__gc = true;
-	__len = true;
+    __index = true;
+    __newindex = true;
+    __call = true;
+    __concat = true;
+    __unm = true;
+    __add = true;
+    __sub = true;
+    __mul = true;
+    __div = true;
+    __mod = true;
+    __pow = true;
+    __tostring = true;
+    __metatable = true;
+    __eq = true;
+    __lt = true;
+    __le = true;
+    __mode = true;
+    __gc = true;
+    __len = true;
 }
 
 
@@ -37,19 +37,19 @@ Object._metainstance.__index = Object
 
 --- Moves defined metamethods into the appropriate instance table
 function Object._metaclass:__newindex(i, v)
-	if metamethods[i] then self._metainstance[i] = v else rawset(self, i, v) end
+    if metamethods[i] then self._metainstance[i] = v else rawset(self, i, v) end
 end
 
 
 --- Creates a new instance when this class is called like a method
 function Object._metaclass:__call(...)
-	return self:wrap({}, ...)
+    return self:wrap({}, ...)
 end
 
 
 --- Returns the class string identifier when instances are cast to string
 function Object:__tostring()
-	return self.classname
+    return self.classname
 end
 
 
@@ -61,49 +61,49 @@ end
 
 --- Class method. Wraps the provided table into a class instance
 function Object:wrap(t, ...)
-	local c = self.class
-	local s = self.super
+    local c = self.class
+    local s = self.super
 
-	if s then s:wrap(t, ...) end
+    if s then s:wrap(t, ...) end
 
-	setmetatable(t, c._metainstance)
-	if rawget(c, "new") then c.new(t, ...) end
-	return t
+    setmetatable(t, c._metainstance)
+    if rawget(c, "new") then c.new(t, ...) end
+    return t
 end
 
 
 --- Class method. Returns true if this class is equal to or inherits from the provided class
 function Object:is(class)
-	local c = self.class
+    local c = self.class
 
-	while c do
-		if c == class then return true end
-		c = c.super
-	end
+    while c do
+        if c == class then return true end
+        c = c.super
+    end
 
-	return false
+    return false
 end
 
 
 --- Class method. Returns a new class that inherits behavior from this class
 function Object:extend()
-	local c = self.class
+    local c = self.class
 
-	local NewClass = {}
+    local NewClass = {}
 
-	NewClass.class = NewClass
-	NewClass.super = c
-	NewClass._metaclass = {}
-	NewClass._metainstance = {}
+    NewClass.class = NewClass
+    NewClass.super = c
+    NewClass._metaclass = {}
+    NewClass._metainstance = {}
 
-	for i, v in pairs(c._metaclass) do NewClass._metaclass[i] = v end
-	NewClass._metaclass.__index = c
-	setmetatable(NewClass, NewClass._metaclass)
+    for i, v in pairs(c._metaclass) do NewClass._metaclass[i] = v end
+    NewClass._metaclass.__index = c
+    setmetatable(NewClass, NewClass._metaclass)
 
-	for i, v in pairs(c._metainstance) do NewClass._metainstance[i] = v end
-	NewClass._metainstance.__index = NewClass
+    for i, v in pairs(c._metainstance) do NewClass._metainstance[i] = v end
+    NewClass._metainstance.__index = NewClass
 
-	return NewClass
+    return NewClass
 end
 
 
